b33c3172b90685bdbdee9c843efbcb1940b9959a
norayr <norayr@arnet.am>
2017-03-03 17:52:28 +0400

    fixed the script; -- noch

--- gimp2ffmpeg/gimp2ffmpeg.py
+++ gimp2ffmpeg/gimp2ffmpeg.py
@@ -26,29 +26,34 @@ print('This is a tool to convert a color curves map from GIMP to a curves filter
 #file = input('Please input the absolute path to the GIMP Color Curve Preset File: ')
 #out = input('Please enter the output file (file will be overwritten if it exists): ')
    #replacing interactive mode with getopt arguments; -- noch
-file=''
-out=''
+in_file=''
+out_file=''
 if len(sys.argv)<2:
   print ("gimp2ffmpeg.py -i <inputfile> -o <outputfile>")
   sys.exit(2)
 try:
-   opts, args = getopt.getopt(sys.argv,"hi:o:",["ifile=","ofile="])
+   args = tuple(sys.argv[1:]) 
+   opts, args = getopt.getopt(args,"hi:o:")
+   print ("opts=", opts)
+   print ("args=", args)
 except getopt.GetoptError:
    print ("gimp2ffmpeg.py -i <inputfile> -o <outputfile>")
    sys.exit(2)
 for opt, arg in opts:
+   print ("opt=", opt)
+   print ("arg=", arg)
    if opt == '-h':
       print ("gimp2ffmpeg.py -i <inputfile> -o <outputfile>")
       sys.exit()
-   elif opt in ("-i", "--ifile"):
-      file = arg
-   elif opt in ("-o", "--ofile"):
-      out = arg
-print ('Input file is "'), file
-print ('Output file is "'), out
+   elif opt == '-i':
+      in_file = arg
+   elif opt == '-o':
+      out_file = arg
+print ('Input file is ', in_file)
+print ('Output file is ', out_file)
 
 #Open the curves file
-curvesfile = open(file,"r")
+curvesfile = open(in_file,"r")
 curvesString = curvesfile.read()
 foundValues = re.findall(r'(?<=samples 256) [\d. ]*',curvesString)
 
@@ -63,7 +68,8 @@ commandPrelim = 'curves=master="'
 command = commandPrelim + ' '.join(masterValues) + '":red="' + ' '.join(redValues) +'":green="' + ' '.join(greenValues) + '":blue="' + ' '.join(blueValues) + '"'
 
 
-with open(out, 'w') as out:
+print (out_file)
+with open(out_file, 'w') as out:
     out.write("Final Command\n\n" + command + '\n\n')
     out.write("master\n\n" + ' '.join(masterValues) + '\n\n')
     out.write("red\n\n" + ' '.join(redValues) + '\n\n')
